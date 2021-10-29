ef96b60de17078c8a8a0134d29ef0d2c0d7c9206
Vyvr <lucianocoddingstuff@gmail.com>
2021-10-29 14:49:32 +0200

    router-added
    added react-router-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,26 @@
 /** @format */
 
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
+
+import Home from "./home/pages/Home";
 
 const App = () => {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
+    <Router>
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+        </Switch>
+      </main>
+    </Router>
   );
 };
 
