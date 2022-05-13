5d9df20954dce8ff00ce1e81007f10cab61497aa
Vyvr <50800058+Vyvr@users.noreply.github.com>
2022-05-13 16:18:13 +0200

    adds some pictures to homepage

--- frontend/src/home/imagesPath.js
+++ frontend/src/home/imagesPath.js
@@ -1,10 +1,29 @@
-import pic1 from './images/1.jpg';
-import pic2 from './images/2.jpg';
-import pic3 from './images/3.jpg';
-import pic4 from './images/4.jpg';
-import pic5 from './images/5.jpg';
-import pic6 from './images/6.jpg';
+import pic1 from "./images/1.jpg";
+import pic2 from "./images/2.jpg";
+import pic3 from "./images/3.jpg";
+import pic4 from "./images/4.jpg";
+import pic5 from "./images/5.jpg";
+import pic6 from "./images/6.jpg";
+import pic7 from "./images/7.jpg";
+import pic8 from "./images/8.jpg";
+import pic9 from "./images/9.jpg";
+import pic10 from "./images/10.jpg";
+import pic11 from "./images/11.jpg";
+import pic12 from "./images/12.jpg";
 
-const imagesPath = [pic1, pic2, pic3, pic4, pic5, pic6];
+const imagesPath = [
+  pic1,
+  pic2,
+  pic3,
+  pic4,
+  pic5,
+  pic6,
+  pic7,
+  pic8,
+  pic9,
+  pic10,
+  pic11,
+  pic12,
+];
 
 export default imagesPath;
